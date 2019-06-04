89722da7e887ed730c67077a56780e028360057c
shfischer <32540427+shfischer@users.noreply.github.com>
2019-06-04 16:09:28 +0100

    added North Sea herring data

--- FLfse/R/data.R
+++ FLfse/R/data.R
@@ -97,3 +97,90 @@
 #'
 #' @source \url{https://www.stockassessment.org}
 "whg4_conf_sam"
+
+
+
+#' North Sea haddock
+#'
+#' Assessment input data for North Sea haddock as used by ICES WGNSSK 2018.
+#' The time series covers the years 1963-2018, with catches up to and
+#' including 2017.
+#'
+#' @format An object of class \code{FLStock}
+#'
+#' @source \url{https://www.stockassessment.org}
+"had4_stk"
+
+#' North Sea haddock indices
+#'
+#' Survey indices for North Sea cod. Two surveys are included: Q1 IBTS and
+#' Q3 IBTS. The time series covers the years 1967-2018 for Q1 and 1991-2017 for
+#' Q3.The survey values are the values as used by WGNSSk 2018 and not the
+#' raw values.
+#'
+#' @format An object of class \code{FLIndices} with elements:
+#' \describe{
+#'   \item{IBTS_Q1_gam}{An object of class \code{FLIndex}}
+#'   \item{IBTS_Q3_gam}{An object of class \code{FLIndex}}
+#' }
+#'
+#' @source \url{https://www.stockassessment.org}
+"had4_idx"
+
+#' North Sea haddock SAM configuration file
+#'
+#' Configuration file for the SAM stockassessment for North Sea haddock. The
+#' configuration is simply the default configuration derived from SAM.
+#'
+#' @source \url{https://www.stockassessment.org}
+"had4_conf_sam"
+
+
+#' North Sea herring
+#'
+#' Herring (Clupea harengus) in Subarea 4 and divisions 3.a and 7.d, autumn
+#' spawners (North Sea, Skagerrak and Kattegat, eastern English Channel).
+#' Assessment input data for North Sea herring as used by ICES HAWG 2019.
+#' The time series covers the years 1947-2019, with catches up to and
+#' including 2018.
+#'
+#' @format An object of class \code{FLStock}
+#'
+#' @source \url{https://github.com/ices-eg/wg_HAWG/tree/master/NSAS}
+"her4_stk"
+
+#' North Sea herring indices
+#'
+#' Herring (Clupea harengus) in Subarea 4 and divisions 3.a and 7.d, autumn
+#' spawners (North Sea, Skagerrak and Kattegat, eastern English Channel).
+#' Survey indices for North Sea herring. Includes data for eight surveys:
+#' HERAS, IBTS-Q1, IBTS0", IBTS-Q3, LAI-ORSH, LAI-BUN, LAI-CNS and LAI-SNS.
+#' The survey values are the values as used by ICES HAWG 2019 and not the
+#' raw values.
+#'
+#' @format An object of class \code{FLIndices} with elements:
+#' \describe{
+#'   \item{HERAS}{An object of class \code{FLIndex}}
+#'   \item{IBTS-Q1}{An object of class \code{FLIndex}}
+#'   \item{IBTS0}{An object of class \code{FLIndex}}
+#'   \item{IBTS-Q3}{An object of class \code{FLIndex}}
+#'   \item{LAI-ORSH}{An object of class \code{FLIndex}}
+#'   \item{LAI-BUN}{An object of class \code{FLIndex}}
+#'   \item{LAI-CNS}{An object of class \code{FLIndex}}
+#'   \item{LAI-SNS}{An object of class \code{FLIndex}}
+#' }
+#'
+#' @source \url{https://github.com/ices-eg/wg_HAWG/tree/master/NSAS}
+"her4_idx"
+
+#' North Sea herring SAM configuration file
+#'
+#' Herring (Clupea harengus) in Subarea 4 and divisions 3.a and 7.d, autumn
+#' spawners (North Sea, Skagerrak and Kattegat, eastern English Channel).
+#' Configuration file for the SAM stockassessment for North Sea herring as used
+#' by ICES HAWG 2019.
+#'
+#' @source \url{https://github.com/ices-eg/wg_HAWG/tree/master/NSAS}
+"her4_conf_sam"
+
+
