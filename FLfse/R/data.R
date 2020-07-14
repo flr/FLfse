d41fc2c221a8710cae66afbc3a78315f3ae650b7
shfischer <32540427+shfischer@users.noreply.github.com>
2020-07-14 18:30:16 +0100

    updated data documentation

--- FLfse/R/data.R
+++ FLfse/R/data.R
@@ -1,212 +1,141 @@
 #' North Sea cod
 #'
-#' Assessment input data for North Sea cod as used by ICES WGNSSK 2018. The time
-#' series covers the years 1963-2018, with catches up to and including 2017.
+#' Cod (\emph{Gadus morhua}) in Subarea 4, Division 7.d, and Subdivision 20
+#' (North Sea, eastern English Channel, Skagerrak).
+#' Assessment input data for North Sea cod as used by ICES WGNSSK 2018 and 2019.
+#' Includes the stock, the indices and the SAM configuration. See the example(s)
+#' below for how to run the assessment.
 #'
-#' @format An object of class \code{FLStock}
+#' @examples
+#' # Replicate the 2018 assessment:
+#' fit <- FLR_SAM(stk = cod4_stk,
+#'                idx = cod4_idx,
+#'                conf = cod4_conf_sam)
+#'
+#' # Replicate the 2019 assessment:
+#' fit <- FLR_SAM(stk = cod4_stk_2019,
+#'                idx = cod4_idx_2019,
+#'                conf = cod4_conf_sam)
 #'
 #' @source \url{https://www.stockassessment.org}
 "cod4_stk"
 
-#' North Sea cod
-#'
-#' Assessment input data for North Sea cod as used by ICES WGNSSK 2019. The time
-#' series covers the years 1963-2019, with catches up to and including 2018.
-#'
-#' @format An object of class \code{FLStock}
-#'
-#' @source \url{https://www.stockassessment.org}
+#' @rdname cod4_stk
 "cod4_stk_2019"
 
-#' North Sea cod indices
-#'
-#' Survey indices for North Sea cod. The time
-#' series covers the years 1963-2018. Two surveys are included: Q1 IBTS and
-#' Q3 IBTS. The survey values are the values as used by WGNSSk 2018 and not the
-#' raw values.
-#'
-#' @format An object of class \code{FLIndices} with elements:
-#' \describe{
-#'   \item{IBTS_Q1_gam}{An object of class \code{FLIndex}}
-#'   \item{IBTS_Q3_gam}{An object of class \code{FLIndex}}
-#' }
-#'
-#' @source \url{https://www.stockassessment.org}
+#' @rdname cod4_stk
+"cod4_stk_2019"
+
+#' @rdname cod4_stk
 "cod4_idx"
 
-#' North Sea cod indices
-#'
-#' Survey indices for North Sea cod. The time
-#' series covers the years 1963-2019. Two surveys are included: Q1 IBTS and
-#' Q3 IBTS. The survey values are the values as used by WGNSSk 2019 and not the
-#' raw values.
-#'
-#' @format An object of class \code{FLIndices} with elements:
-#' \describe{
-#'   \item{IBTS_Q1_gam}{An object of class \code{FLIndex}}
-#'   \item{IBTS_Q3_gam}{An object of class \code{FLIndex}}
-#' }
-#'
-#' @source \url{https://www.stockassessment.org}
+#' @rdname cod4_stk
 "cod4_idx_2019"
 
-#' North Sea cod SAM configuration file
-#'
-#' Configuration file for the SAM stockassessment for North Sea cod as used by
-#' WGNSSK 2018.
-#'
-#' @source \url{https://www.stockassessment.org}
+#' @rdname cod4_stk
 "cod4_conf_sam"
 
 #' Irish Sea plaice
 #'
+#' Plaice (\emph{Pleuronectces platessa}) in Division 7.a (Irish Sea).
 #' Assessment input data for Irish Sea plaice as used by ICES WGCSE 2017.
-#' The time series covers the years 1963-2016.
+#' Includes the stock, the indices and the SAM configuration. See the example
+#' below for how to run the assessment.
 #'
-#' @format An object of class \code{FLStock}
+#' @examples
+#' # Replicate the 2017 assessment:
+#' \dontrun{
+#' fit <- FLR_SAM(stk = ple7a_stk,
+#'                idx = ple7a_idx,
+#'                conf = ple7a_conf_sam)
+#' }
+#'
 "ple7a_stk"
 
-#' Irish Sea plaice indices
-#'
-#' Survey indices for Irish Sea plaice as used by WGCSE 2017. Three survey
-#' indices are included: "UK BT SURVEY (Sept) - all stations Rcode", "DARDS"
-#' and "DARDA".
-#'
-#' @format An object of class \code{FLIndices} with elements:
-#' \describe{
-#'   \item{UK BT SURVEY (Sept) - all stations Rcode}{An object of class
-#'     \code{FLIndex}}
-#'   \item{DARDS}{An object of class \code{FLIndex}}
-#'   \item{DARDA}{An object of class \code{FLIndex}}
-#' }
+#' @rdname ple7a_stk
 "ple7a_idx"
 
-#' Irish Sea plaice SAM configuration file
-#'
-#' Configuration file for the SAM stockassessment for Irish Sea plaice as used
-#' by WGCSE 2017.
+#' @rdname ple7a_stk
 "ple7a_conf_sam"
-
 
 
 #' North Sea whiting
 #'
+#' Whiting (\emph{Merlangius merlangus}) in Subarea 4 and Division 7.d
+#' (North Sea and eastern English Channel).
 #' Assessment input data for North Sea whiting as used by ICES WGNSSK 2018.
-#' The time series covers the years 1978-2018, with catches up to and
-#' including 2017.
-#'
-#' @format An object of class \code{FLStock}
+#' Includes the stock, the indices and the SAM configuration. See the example
+#' below for how to run the assessment.
 #'
 #' @source \url{https://www.stockassessment.org}
+#'
+#' #' @examples
+#' # Replicate the 2017 assessment:
+#' fit <- FLR_SAM(stk = whg4_stk,
+#'                idx = whg4_idx,
+#'                conf = whg4_conf_sam)
+#'
 "whg4_stk"
 
-#' North Sea whiting indices
-#'
-#' Survey indices for North Sea whiting The time
-#' series covers the years 1983-2018. Two surveys are included: Q1 IBTS and
-#' Q3 IBTS. The survey values are the values as used by WGNSSk 2018 and not the
-#' raw values.
-#'
-#' @format An object of class \code{FLIndices} with elements:
-#' \describe{
-#'   \item{IBTS-Q1}{An object of class \code{FLIndex}}
-#'   \item{IBTS-Q3}{An object of class \code{FLIndex}}
-#' }
-#'
-#' @source \url{https://www.stockassessment.org}
+#' @rdname whg4_stk
 "whg4_idx"
 
-#' North Sea whiting SAM configuration file
-#'
-#' Configuration file for the SAM stockassessment for North Sea whiting as used
-#'  by WGNSSK 2018.
-#'
-#' @source \url{https://www.stockassessment.org}
+#' @rdname whg4_stk
 "whg4_conf_sam"
-
 
 
 #' North Sea haddock
 #'
+#' Haddock (\emph{Melanogrammus aeglefinus}) in Subarea 4, Division 6.a, and
+#' Subdivision 20 (North Sea, West of Scotland, Skagerrak).
 #' Assessment input data for North Sea haddock as used by ICES WGNSSK 2018.
-#' The time series covers the years 1963-2018, with catches up to and
-#' including 2017.
-#'
-#' @format An object of class \code{FLStock}
+#' Includes the stock, the indices and the SAM configuration. See the example
+#' below for how to run the assessment.
 #'
 #' @source \url{https://www.stockassessment.org}
+#'
+#' @examples
+#' # Replicate the 2018 assessment:
+#' fit <- FLR_SAM(stk = had4_stk,
+#'                idx = had4_idx,
+#'                conf = had4_stk)
+#'
 "had4_stk"
 
-#' North Sea haddock indices
-#'
-#' Survey indices for North Sea cod. Two surveys are included: Q1 IBTS and
-#' Q3 IBTS. The time series covers the years 1967-2018 for Q1 and 1991-2017 for
-#' Q3.The survey values are the values as used by WGNSSk 2018 and not the
-#' raw values.
-#'
-#' @format An object of class \code{FLIndices} with elements:
-#' \describe{
-#'   \item{IBTS_Q1_gam}{An object of class \code{FLIndex}}
-#'   \item{IBTS_Q3_gam}{An object of class \code{FLIndex}}
-#' }
-#'
-#' @source \url{https://www.stockassessment.org}
+#' @rdname had4_stk
 "had4_idx"
 
-#' North Sea haddock SAM configuration file
-#'
-#' Configuration file for the SAM stockassessment for North Sea haddock. The
-#' configuration is simply the default configuration derived from SAM.
-#'
-#' @source \url{https://www.stockassessment.org}
+#' @rdname had4_stk
 "had4_conf_sam"
-
 
 #' North Sea herring
 #'
-#' Herring (Clupea harengus) in Subarea 4 and divisions 3.a and 7.d, autumn
-#' spawners (North Sea, Skagerrak and Kattegat, eastern English Channel).
+#' Herring (Clupea harengus) in Subarea 4 and divisions 3.a and 7.d,
+#' autumn spawners (North Sea, Skagerrak and Kattegat, eastern English Channel).
 #' Assessment input data for North Sea herring as used by ICES HAWG 2019.
-#' The time series covers the years 1947-2019, with catches up to and
-#' including 2018.
+#' Includes the stock, the indices and the SAM configuration.
 #'
-#' @format An object of class \code{FLStock}
+#' Please note that the herring SAM assessment requires a different version of
+#' the stockassessment package. For details, see the readme at
+#' https://github.com/shfischer/FLfse
 #'
 #' @source \url{https://github.com/ices-eg/wg_HAWG/tree/master/NSAS}
-"her4_stk"
-
-#' North Sea herring indices
 #'
-#' Herring (Clupea harengus) in Subarea 4 and divisions 3.a and 7.d, autumn
-#' spawners (North Sea, Skagerrak and Kattegat, eastern English Channel).
-#' Survey indices for North Sea herring. Includes data for eight surveys:
-#' HERAS, IBTS-Q1, IBTS0", IBTS-Q3, LAI-ORSH, LAI-BUN, LAI-CNS and LAI-SNS.
-#' The survey values are the values as used by ICES HAWG 2019 and not the
-#' raw values.
-#'
-#' @format An object of class \code{FLIndices} with elements:
-#' \describe{
-#'   \item{HERAS}{An object of class \code{FLIndex}}
-#'   \item{IBTS-Q1}{An object of class \code{FLIndex}}
-#'   \item{IBTS0}{An object of class \code{FLIndex}}
-#'   \item{IBTS-Q3}{An object of class \code{FLIndex}}
-#'   \item{LAI-ORSH}{An object of class \code{FLIndex}}
-#'   \item{LAI-BUN}{An object of class \code{FLIndex}}
-#'   \item{LAI-CNS}{An object of class \code{FLIndex}}
-#'   \item{LAI-SNS}{An object of class \code{FLIndex}}
+#' #' @examples
+#' \dontrun{
+#' # NOTE: the herring SAM assessment requires a different version of
+#' # the stockassessment package
+#' fit <- FLR_SAM(stk = her4_stk,
+#'                idx = her4_idx,
+#'                conf = her4_conf_sam)
 #' }
 #'
-#' @source \url{https://github.com/ices-eg/wg_HAWG/tree/master/NSAS}
+"her4_stk"
+
+#' @rdname her4_stk
 "her4_idx"
 
-#' North Sea herring SAM configuration file
-#'
-#' Herring (Clupea harengus) in Subarea 4 and divisions 3.a and 7.d, autumn
-#' spawners (North Sea, Skagerrak and Kattegat, eastern English Channel).
-#' Configuration file for the SAM stockassessment for North Sea herring as used
-#' by ICES HAWG 2019.
-#'
-#' @source \url{https://github.com/ices-eg/wg_HAWG/tree/master/NSAS}
+#' @rdname her4_stk
 "her4_conf_sam"
 
 
@@ -215,7 +144,8 @@
 #' Mackerel  (\emph{Scomber  scombrus}) in subareas 1-8 and 14, and in
 #' Division 9.a (the Northeast Atlantic and adjacent waters).
 #' Assessment input data for Northeast Atlantic mackerel as used by ICES WGWIDE
-#' 2019. Includes the stock, the indices and the SAM configuration.
+#' 2019. Includes the stock, the indices and the SAM configuration. See the
+#' example below for how to run the assessment.
 #'
 #' \code{mac.27.nea_stk_2019} includes tagging data, stored as an attribute in
 #' \code{attr(catch.n(mac.27.nea_stk_2019), "recap")}. An additional tagging
